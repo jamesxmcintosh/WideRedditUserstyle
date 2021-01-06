7abbab99cfb2f832860268332ae2265db1511117
jxm <jamesxmcintosh@users.noreply.github.com>
2021-01-06 12:07:50 +1100

    Initial commit

---
+++ WideRedditCSS.user.js
@@ -0,0 +1,16 @@
+/* ==UserStyle==
+@name         Wide new reddit | Userstyle
+@namespace https://greasyfork.org/en/users/195080-jxm
+@description  Wide view for new reddit. Now with card view!
+@version     1.2
+@license     GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+@icon        https://www.redditstatic.com/desktop2x/img/favicon/favicon-96x96.png
+@author      jxm
+@preprocessor stylus
+==/UserStyle== */
+@-moz-document domain("reddit.com")
+._1OVBBWLtHoSPfGCRaPzpTf._3nSp9cdBpqL13CqjdMr2L_ { width: 90% !important;
+    }
+div  {
+    max-width: none !important;
+}
